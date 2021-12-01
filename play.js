696f12f1ab4ef7bca53557ab6c0635e4294b910f
Micheala Palmer <michealapalmer@gmail.com>
2021-12-01 16:58:11 -0500

    connection module

--- play.js
+++ play.js
@@ -1,28 +1,6 @@
-const net = require("net");
+
+const {connect} = require("./client");
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-  //handling incoming data
-  conn.on('data', (data)=> {
-    console.log(data.toString());
-    //received you ded cuz you idled
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //when connect to the server, it will listen to event and trigger that callback
-  conn.on('connect', () => {
-    console.log("Connected to server");
-    //conn.write(data);
-  });
-
-  return conn;
-  
-};
-
 console.log("Connecting ...");
 connect();
